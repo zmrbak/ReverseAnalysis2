cc77a7f2db3687ab3febd505729189beb6e41144
zmrbak <20995658@qq.com>
2021-05-20 18:00:58 +0800

    add code

---
+++ 配套代码/L001/L001/L001.cpp
@@ -0,0 +1,10 @@
+﻿// L001.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+	printf("Hello World!\n");
+}
+
---
+++ 配套代码/L005/RE005/RE005.cpp
@@ -0,0 +1,18 @@
+﻿// RE005.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int f_add(uint32_t sz,int *a1,int* a2,int* a3)
+{
+	for (size_t i = 0; i < sz; i++)
+	{
+		a3[i] = a1[i] + a2[i];
+	}
+	return 0;
+}
+
+int main()
+{
+	printf("Hello World!\n");
+}
