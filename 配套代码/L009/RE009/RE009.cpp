0dc19626fdd2e5021be4daeb9f8cb1f8684edec9
zmrbak <20995658@qq.com>
2021-05-23 18:15:36 +0800

    add code

---
+++ 配套代码/L009/RE009/RE009.cpp
@@ -0,0 +1,17 @@
+﻿// RE009.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int f(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10)
+{
+	return a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10;
+}
+
+
+int main()
+{
+	printf("Hello World!\n");
+	int a = f(1,2,3,4,5,6,7,8,9,10);
+	return 0;
+}
