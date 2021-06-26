aa54bb3c329bd3aec78804bf3f6aa9e93c9b4f64
zmrbak <20995658@qq.com>
2021-06-26 09:46:17 +0800

    add code

---
+++ 配套代码/L028/RE028/RE028.cpp
@@ -0,0 +1,31 @@
+﻿// RE028.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+void f(int* a1, int* a2, size_t count)
+{
+	for (size_t i = 0; i < count / 7; i++)
+	{
+		a1[i * 3] = a2[i * 7];
+	}
+}
+
+void f1(int* a1, int* a2, size_t count)
+{
+	int x1 = 0;
+	int x2 = 0;
+	size_t cnt = count / 7;
+	for (size_t i = 0; i < cnt; i++)
+	{
+		//a1[i * 3] = a2[i * 7];
+		a1[x1] = a2[x2];
+		x1 += 3;
+		x2 += 7;
+	}
+}
+
+int main()
+{
+
+}
