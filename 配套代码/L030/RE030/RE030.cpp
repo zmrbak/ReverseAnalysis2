f1f26663801478518df7504c7395ca93bfb9f3b4
zmrbak <20995658@qq.com>
2021-06-27 10:13:15 +0800

    add code

---
+++ 配套代码/L030/RE030/RE030.cpp
@@ -0,0 +1,23 @@
+﻿// RE030.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int f_signed(int a)
+{
+	//return a / 3;
+	//return a / 8;
+	return a / 9;
+}
+
+uint32_t f_unsigned(uint32_t a)
+{
+	/*return a / 3;*/
+	//return a / 8;
+	return a / 9;
+}
+
+int main()
+{
+   
+}
