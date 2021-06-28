c90c7f2ebf9f569b99cb07cb50cb3401c2bcf293
zmrbak <20995658@qq.com>
2021-06-28 19:33:15 +0800

    add code

---
+++ 配套代码/L031/RE031/RE031.cpp
@@ -0,0 +1,43 @@
+﻿// RE031.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int atoi_1(const char* s)
+{
+	int result = 0;
+	int neg = 0;
+	if (*s == '-')
+	{
+		neg = 1;
+		s++;
+	}
+
+	while (*s)
+	{
+		if (*s < '0' || *s >'9')
+		{
+			printf("Error! Unexpected char: '%c'\n",*s);
+			exit(0);
+		}
+		/*result = result * 10 + *s - '0';
+		s++;*/
+		result = result * 10 + (*s++) - '0';
+	}
+
+	if (neg == 1)
+	{
+		result = -result;
+	}
+	return result;
+}
+
+int main()
+{
+	//\0作为结尾 0
+	char* data1 = (char*)"-1234";
+	int a1 = atoi_1(data1);
+
+	const char* data2 = "-1234";
+	int a2 = atoi_1(data2);
+}
