78f744d4f75cb20e87da56a231a792c4be6c9ee9
zmrbak <20995658@qq.com>
2021-06-29 15:55:36 +0800

    add code

---
+++ 配套代码/L032/RE032/RE032.cpp
@@ -0,0 +1,33 @@
+﻿// RE032.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+inline int f2c(int f)
+{
+	return (f - 32) * 5 / 9;
+}
+//return main(__argc, __argv, _get_initial_narrow_environment());
+int main(int argc, char* argv[])
+{
+	//strcpy()
+	//strcmp()
+	//strlen()
+	//memset()
+	//memcmp()
+	//memcmp()
+	int b = atoi(argv[1]);
+	int a = f2c(b);
+	printf("%d\n", a);
+
+	const char* data = "12345";
+	char* data2 = strcpy(argv[1], data);
+	printf("%s\n", data2);
+
+	int c=strcmp(data, data2);
+	printf("%d\n", c);
+
+	int d=strlen(data2);
+	printf("%d\n", d);
+}
